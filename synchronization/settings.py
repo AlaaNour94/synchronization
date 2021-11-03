2478b883d56574eef30a946b3e3a3a32976e5c11
Alaa <alaanourali@gmail.com>
2021-11-03 21:31:47 +0200

    allow debug

--- synchronization/settings.py
+++ synchronization/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bkak9jupcv4by5nee+1gv@e_k%qoo$(uapahtzdwmvjj6wwppg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
